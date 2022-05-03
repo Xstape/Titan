99ea374e5bb4992c35b26c36f46f55fd92d7a179
Xstape <xstape@MacBook-Pro-Roman.local>
2022-05-03 03:32:04 +0300

    begin fix main cycle

--- js/script.js
+++ js/script.js
@@ -23,11 +23,19 @@ const openControl = document.getElementById('openControl'),
 
 async function delayedCycle() {
     let waterLevelHeight = 0;
-    for (step = 0; step <= 1000; step++) {   
+    // for (step = 0; step <= 1000; step++) {   
+    //     content('#L', waterLevelHeight);
+    //     // Расчет шага роста уровня воды с учетом степени открытия вентиля
+    //     waterLevelHeight += maxFillingSpeed * (openControl.value / 100);
+    //     // Конвертация и вывод высоты уровня воды в данную секунду
+    //     waterLevel.style.height = level - waterLevelHeight * (level / 2000) + "px";
+    //     await sleep(1000);
+    // }
+    // [TODO] — объем воды на цифровом датчике не должен превышать объем бака
+    // Цикл должен возобновляться при снижении уровня/не достижении уровнем максимума
+    while ((level - waterLevelHeight * (level / 2000)) > 0) {
         content('#L', waterLevelHeight);
-        // Расчет шага роста уровня воды с учетом степени открытия вентиля
         waterLevelHeight += maxFillingSpeed * (openControl.value / 100);
-        // Конвертация и вывод высоты уровня воды в данную секунду
         waterLevel.style.height = level - waterLevelHeight * (level / 2000) + "px";
         await sleep(1000);
     }
